992c1803ba4c3df30cfcda0b3b72b1417aa3bded
Mannan <mannan@Mannans-MacBook-Air.local>
2022-07-27 16:48:16 +0530

    wallet connect feature

--- src/components/pages/HomePage/Home.js
+++ src/components/pages/HomePage/Home.js
@@ -1,10 +1,24 @@
-import { Button, Stack } from "@mui/material";
-import React from "react";
-import { CardList } from "./CardList";
-import { Header } from "./Header";
-import { Link } from "react-router-dom";
+import { Button, Stack } from '@mui/material';
+import React from 'react';
+import { useEffect, useState } from 'react';
+import { useWeb3React } from '@web3-react/core';
+import { connectors } from '../../../connector';
+import { toHex, truncateAddress } from '../../../utils';
+
+import { CardList } from './CardList';
+import { Header } from './Header';
+import { Link } from 'react-router-dom';
+import { Walletconnect } from '../WalletConnect';
 
 function Home() {
+  const { library, chainId, account, activate, deactivate, active } =
+    useWeb3React();
+
+  useEffect(() => {
+    const provider = window.localStorage.getItem('provider');
+    console.log(provider);
+    if (provider) activate(connectors[provider]);
+  }, []);
   return (
     <Stack className="container">
       <Header />
@@ -16,15 +30,16 @@ function Home() {
           <Button
             variant="contained"
             sx={{
-              marginTop: "16px",
-              textTransform: "unset",
+              marginTop: '16px',
+              textTransform: 'unset',
               width: 150,
-              backgroundColor: "#349FFF",
+              backgroundColor: '#349FFF',
             }}
           >
             Join waitlist
           </Button>
         </Link>
+        <Walletconnect />
       </div>
       <CardList />
     </Stack>
---
+++ src/connector.js
@@ -0,0 +1,26 @@
+import { InjectedConnector } from '@web3-react/injected-connector';
+import { WalletConnectConnector } from '@web3-react/walletconnect-connector';
+import { WalletLinkConnector } from '@web3-react/walletlink-connector';
+
+const injected = new InjectedConnector({
+  supportedChainIds: [1],
+});
+
+//https://mainnet.infura.io/v3/35510359f7784e289951df662325c6ac
+
+const walletconnect = new WalletConnectConnector({
+  rpcUrl: `https://mainnet.infura.io/v3/${process.env.REACT_APP_INFURA_KEY}`,
+  bridge: 'https://bridge.walletconnect.org',
+  qrcode: true,
+});
+
+const walletlink = new WalletLinkConnector({
+  url: `https://mainnet.infura.io/v3/${process.env.REACT_APP_INFURA_KEY}`,
+  appName: 'Web3 Testers',
+});
+
+export const connectors = {
+  injected: injected,
+  walletConnect: walletconnect,
+  coinbaseWallet: walletlink,
+};
